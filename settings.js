ab9fb8a5712bfe7a06f691d2fc7c4f5a1e670662
Tasif78-engeee <tasifahmed853@gmail.com>
2025-09-16 02:12:34 +0600

    Update settings.
    Update bot

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: LM BOT,
+  author: '‎',TASIF
+  botName: "LM BOT",
+  botOwner: 'TASIF, // Your name
+  ownerNumber: '+880 01308991817', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
